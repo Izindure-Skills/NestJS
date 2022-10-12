3fff70c9bf057ef7eb80becfeb9bc8517f120c5f
Nick Eric Kayumba <kericox@gmail.com>
2022-10-12 06:35:41 +0200

    minor change on index.ts

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -15,9 +15,14 @@ axios.get(url).then(response => {
   const title = todo.title;
   const completed = todo.completed;
 
+  logTodo(id, title, completed)
+
+});
+
+const logTodo = (id: number, title:string, completed:boolean) => {
   console.log(`
-  The Tod with id: ${id}
+  The Todo with id: ${id}
   Has a title of: ${title}
   Is it finished? ${completed}
   `);
-});
+};
